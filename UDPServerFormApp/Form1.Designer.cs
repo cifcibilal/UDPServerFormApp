957ef426397f89a7e2310e2b215beb8da53bd20d
cifcibilal <cifci.bilal@outlook.com>
2023-08-24 09:09:22 +0300

    Proje dosyası ekle.

---
+++ UDPServerFormApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace UDPServerFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServerPort = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Port :";
+            // 
+            // txtServerPort
+            // 
+            this.txtServerPort.Location = new System.Drawing.Point(133, 14);
+            this.txtServerPort.Name = "txtServerPort";
+            this.txtServerPort.Size = new System.Drawing.Size(153, 22);
+            this.txtServerPort.TabIndex = 1;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(16, 47);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(409, 156);
+            this.txtLog.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(292, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(133, 32);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 218);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtServerPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServerPort;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ UDPServerFormApp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDPServerFormApp
+{
+    public partial class Form1 : Form
+    {
+        UdpClient server;
+        IPEndPoint endPoint;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            server = new UdpClient(int.Parse(txtServerPort.Text));
+            endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Thread thread = new Thread(new ThreadStart(ServerStart));
+            thread.Start();
+            btnStart.Enabled = false;
+
+            WriteLog("Server Başlatıldı");
+            WriteLog("-----------------------------------------------");
+        }
+
+        private void ServerStart()
+        {
+            while (true) 
+            {
+                byte[] buffer = server.Receive(ref endPoint);
+
+                string[] msg = Encoding.UTF8.GetString(buffer).Split('/');
+
+                int clientPort = int.Parse(msg[0]);
+                string clientHostName = msg[1];
+                string request = msg[2];
+
+                WriteLog($"{clientPort} numaralı istemci portu, {clientHostName}-> Servera ->\n {request}");
+
+                string response = $"{clientPort} portundan veri iletildi.";
+
+                buffer = Encoding.UTF8.GetBytes(response);
+                server.Send(buffer,buffer.Length,clientHostName,clientPort);
+
+                WriteLog($"Bilgi: {clientPort} Serverdan {clientHostName} numaralı istemciye cevap gönderildi.");
+                WriteLog("---------------------------------------------------------------------------------");
+
+            }
+        }
+
+        private void WriteLog(string msg)
+        {
+            MethodInvoker invoker = new MethodInvoker(delegate
+            {
+                txtLog.Text += $"{msg}.{Environment.NewLine}";
+            });
+            this.BeginInvoke(invoker);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
